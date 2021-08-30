9cffc7f80866bb02929d2c2031b532bef0f10f26
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-08-30 12:38:04 +0200

    parte visual: nuevo producto & agregar

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar nuevo producto
+                        </h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre producto"
+                                    name="nombre"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Precio</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio"
+                                    name="precio"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Agregar</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NuevoProducto;
